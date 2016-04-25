206fc5dd40d44b849b154bceb49666b30038d4b6
Leo Jin <pacjin@gmal.com>
2016-04-25 11:16:11 -0400

    another round

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,58 @@
 ///<reference path="../typings/main.d.ts" />
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {
+    Router, Route,
+    hashHistory, Link,
+    IndexRoute
+} from 'react-router';
+
+class Application extends React.Component<{}, void> {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+class About extends React.Component<{}, void> {
+    constructor(){
+        super();
+    }
+
+    render(){
+        return (
+            <h1>About</h1>
+        );
+    }
+}
+
+class Home extends React.Component<{}, void> {
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <div>
+                <h1>home</h1>
+                <Link to="/about">About</Link>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-    <h1>Hello</h1>,
+    <Router history={hashHistory}>
+        <Route path="/" component={Application}>
+            <IndexRoute component={Home}/>
+            <Route path="/about" component={About}/>
+        </Route>
+    </Router>,
     document.getElementById("bootstrapContainer")
 );
